af32188a59ca4a3136f67b38f2e565591e41668b
cthuba <cthuba8@gmail.com>
2023-07-14 07:54:34 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - prints the alphabet.
+ * main - printsthe alphabet.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int i;
+
 for (i = 0; i < 52; i++)
 {
-putchar(alp[i]);
+	putchar(alp[i]);
 }
 putchar('\n');
 return (0);
